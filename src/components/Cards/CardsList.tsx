71d09461801920909a5b98b54f31205440897bb7
yolf <gleoros@gmail.com>
2022-10-23 14:26:35 +0300

    refactor: refactor code

--- src/components/Cards/CardsList.tsx
+++ src/components/Cards/CardsList.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { CardListProp } from './types';
 import Card from './Card';
 import styles from './Cards.module.scss';
+import { v4 as uuidv4 } from 'uuid';
 
 export default class CardsList extends React.Component<CardListProp> {
   render() {
     return (
       <div className={styles.cards} data-testid="cards-list">
-        {this.props.cards.map((card, index) => (
-          <Card key={card.id || index} {...card} />
+        {this.props.cards.map((card) => (
+          <Card key={card.id || uuidv4()} {...card} />
         ))}
       </div>
     );
